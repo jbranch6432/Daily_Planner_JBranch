318db0da5edaf9b1c356b1cdcd6bfb2a5b5d9dc6
Jamie Branch <schwung988@yahoo.com>
2023-10-01 22:44:01 -0400

    updated README

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
+$(document).ready(function () {
+
+//listener added to to save the events entered by the user
 $('.saveBtn').on('click', function() {
   var value = $(this).siblings('.description').val();
   var key = $(this).parent().attr('id');
@@ -24,11 +16,7 @@ $('.saveBtn').on('click', function() {
   console.log(localStorage);
 });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  //Function will change the background color of each time block according to whether it is in the past, the present, or the future
   function hourCurrent() {
     var currentHour = dayjs().hour();
 
@@ -52,11 +40,8 @@ $('.saveBtn').on('click', function() {
   //check if current time needs to be updated
   var update = setInterval(hourCurrent, 1000);
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 
-  //load the saved data from the localStorage
+  //load the saved data from the localStorage to the page
   $('#hour-9 .description').val(localStorage.getItem('hour-9'))
   $('#hour-10 .description').val(localStorage.getItem('hour-10'))
   $('#hour-11 .description').val(localStorage.getItem('hour-11'))
@@ -67,7 +52,6 @@ $('.saveBtn').on('click', function() {
   $('#hour-16 .description').val(localStorage.getItem('hour-16'))
   $('#hour-17 .description').val(localStorage.getItem('hour-17'))
 
-  // TODO: Add code to display the current date in the header of the page.
-
+//Displays the current date to the header of the page
   $('#currentDay').text(dayjs().format('MM/DD/YYYY') )
 });
